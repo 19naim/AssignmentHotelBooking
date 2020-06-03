3719a85aa83e5c7c4bb9bc0438e04a05551fd827
Md Naim Hossain Arif <thk.naim@gmail.com>
2020-06-03 13:25:22 +0200

    bugfix

--- src/com/company/RoomType.java
+++ src/com/company/RoomType.java
@@ -1,12 +1,4 @@
 package com.company;
 
 public class RoomType {
-    private String singleRoom;
-    private int numOfSingleRoom;
-
-    private String doubleRoom;
-    private int numOfDoubleRoom;
-
-    private String businessRoom;
-    private int numOfBusinessRoom;
 }
